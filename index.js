ea48e47035cda82acccd4a1821a435cb8ad572e5
createaihubai-tech <createaihub.ai@gmail.com>
2026-01-18 13:44:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Server running");
+  res.send("CreateAIHub.ai – Website under construction");
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
