c7fa445004dc59d651fbca94cf5804274c69b131
createaihubai-tech <createaihub.ai@gmail.com>
2026-01-19 08:49:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const express = require("express");
-const app = express();
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-app.get("/", (req, res) => {
-  res.send("CreateAIHub.ai – Website under construction");
-});
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC3RYM58nbXVs2Ny1HhS4ILT1NIXIWeqK4",
+  authDomain: "createaihub.firebaseapp.com",
+  projectId: "createaihub",
+  storageBucket: "createaihub.firebasestorage.app",
+  messagingSenderId: "808253173890",
+  appId: "1:808253173890:web:46cd29aa93e1b8d45ec49b",
+  measurementId: "G-L5ZQ2P84G3"
+};
 
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
